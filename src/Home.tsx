e31ec64e808e4b81c5eb74f9fdace4cab7b670b5
r0bban <54702466+r0bban@users.noreply.github.com>
2024-07-03 22:26:38 +0200

    minor change

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Räkna rätt på bilen!!</h1>
+      <h1>Räkna rätt på bilen!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
